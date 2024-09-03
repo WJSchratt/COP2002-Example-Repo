67d7c1f15ee9bd1ded703f4c3915fbe13a49efd4
WJSchratt <john.schratt1@gmail.com>
2024-09-03 14:34:30 -0400

    added print statement

---
+++ Desktop/github-ex/github.py
@@ -0,0 +1,6 @@
+Python 3.12.5 (v3.12.5:ff3bc82f7c9, Aug  7 2024, 05:32:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Creating a project for GitHub
+... # By Walter Schratt
+>>> print("COP2002 Fall 2024")
+COP2002 Fall 2024
